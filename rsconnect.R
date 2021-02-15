0d1324d580b015ca6f1718ba0602ec2f7100dbe1
Sungpil Han <pil.han@gmail.com>
2021-02-15 16:27:38 +0900

    update: pk1c

--- rsconnect.R
+++ rsconnect.R
@@ -1,4 +1,13 @@
 devtools::install_github("asancpt/NonCompart")
 devtools::install_github("asancpt/ncar")
 
-rsconnect::deployApp(appDir = "~/asancpt/pk1c", appName = "pk1c", account = "asan")
+rsconnect::setAccountInfo(
+  name='mang',
+  token='65F256F8E4D2458F00C479B0FBCEA680',
+  secret='m2EYypi2U4SAS067YbwdF+DHwdEekL381b1TkLlP'
+)
+
+rsconnect::setAccountInfo(name='asan', token='12696AD7B41ABFC67799183BF75ED7CF', secret='G+dZqQWocJu2ZFeFoWV6wQgYopp5YXdXh8+IbFYo')
+rsconnect::deployApp(appName = "pk1ctemp", account = "mang")
+
+
